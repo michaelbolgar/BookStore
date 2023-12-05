5c71b674700626c70e69a51970b1ed31283d9d85
Alexander Altman <qewhouse@gmail.com>
2023-12-05 18:53:29 +0300

    Refactor #1

--- BookStore/BookStore/Moduls/MainTabBar/MainTabBarController.swift
+++ BookStore/BookStore/Moduls/MainTabBar/MainTabBarController.swift
@@ -9,32 +9,35 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
+    //MARK: - Dependencies
     private var shouldAnimateSelectedTabBarItem = false
-
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         generateTabBar()
         animateSelectedTabBarItem()
         delegate = self
     }
-
-        private func generateTabBar() {
-            tabBar.backgroundColor = UIColor.customLightGray
-            viewControllers = [
-                makeVC(vc: NavigationViewController(rootViewController: HomeVC()), image: UIImage(named: "home_unselected"), selectedImage: UIImage(named:"home_selected"), title: "Home"),
-                makeVC(vc: NavigationViewController(rootViewController: CategoriesVC()), image: UIImage(named: "categories_unselected"), selectedImage: UIImage(named: "categories_selected"), title: "Categories"),
-                makeVC(vc: NavigationViewController(rootViewController: LikesVC()), image: UIImage(named: "heart_unselected"), selectedImage: UIImage(named: "heart_selected"), title: "Likes"),
-                makeVC(vc: NavigationViewController(rootViewController: AccountVC()), image: UIImage(named: "account_unselected"), selectedImage: UIImage(named: "account_selected"), title: "Account"),
-            ]
-
-                let tabBarItemAppearance = UITabBarItem.appearance()
-                let attributes: [NSAttributedString.Key: Any] = [
-                    .font: UIFont.openSansBold(ofSize: 14)!,
-                    .foregroundColor: UIColor.black
-                ]
-                tabBarItemAppearance.setTitleTextAttributes(attributes, for: .normal)
-                tabBarItemAppearance.setTitleTextAttributes(attributes, for: .selected)
-        }
+    
+    //MARK: - Private methods
+    private func generateTabBar() {
+        tabBar.backgroundColor = UIColor.customLightGray
+        viewControllers = [
+            makeVC(vc: NavigationViewController(rootViewController: HomeVC()), image: UIImage(named: "home_unselected"), selectedImage: UIImage(named:"home_selected"), title: "Home"),
+            makeVC(vc: NavigationViewController(rootViewController: CategoriesVC()), image: UIImage(named: "categories_unselected"), selectedImage: UIImage(named: "categories_selected"), title: "Categories"),
+            makeVC(vc: NavigationViewController(rootViewController: LikesVC()), image: UIImage(named: "heart_unselected"), selectedImage: UIImage(named: "heart_selected"), title: "Likes"),
+            makeVC(vc: NavigationViewController(rootViewController: AccountVC()), image: UIImage(named: "account_unselected"), selectedImage: UIImage(named: "account_selected"), title: "Account"),
+        ]
+        
+        let tabBarItemAppearance = UITabBarItem.appearance()
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.openSansBold(ofSize: 14) ?? UIFont.systemFont(ofSize: 14),
+            .foregroundColor: UIColor.black
+        ]
+        tabBarItemAppearance.setTitleTextAttributes(attributes, for: .normal)
+        tabBarItemAppearance.setTitleTextAttributes(attributes, for: .selected)
+    }
     
     private func makeVC(vc: UIViewController, image: UIImage?, selectedImage: UIImage?, title: String) -> UIViewController {
         let offset: CGFloat = 5
@@ -63,17 +66,18 @@ final class MainTabBarController: UITabBarController {
         let tabBarButton = tabBar.subviews[selectedIndex + 1]
         
         UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1.0, options: [.curveEaseInOut], animations: {
-                tabBarButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
-            }) { _ in
-                UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1.0, options: [.curveEaseInOut], animations: {
-                    tabBarButton.transform = CGAffineTransform.identity
-                }, completion: nil)
-            }
+            tabBarButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        }) { _ in
+            UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1.0, options: [.curveEaseInOut], animations: {
+                tabBarButton.transform = CGAffineTransform.identity
+            }, completion: nil)
+        }
         
         shouldAnimateSelectedTabBarItem = false
     }
 }
 
+//MARK: - UITabBarControllerDelegate
 extension MainTabBarController: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         shouldAnimateSelectedTabBarItem = true
