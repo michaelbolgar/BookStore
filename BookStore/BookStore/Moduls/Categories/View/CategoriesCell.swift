cf5fc0e60d5ebc34ab0693f5f1289ebf4f399058
Maksimova Kristina <kris.maks.t.7@gmail.ru>
2023-12-06 21:42:26 +0300

    create CategoriesVC

---
+++ BookStore/BookStore/Moduls/Categories/View/CategoriesCell.swift
@@ -0,0 +1,8 @@
+//
+//  CategoriesCell.swift
+//  BookStore
+//
+//  Created by Кристина Максимова on 06.12.2023.
+//
+
+import Foundation
---
+++ BookStore/BookStore/Moduls/Categories/View/CategoriesHeader.swift
@@ -0,0 +1,8 @@
+//
+//  CategoriesHeader.swift
+//  BookStore
+//
+//  Created by Кристина Максимова on 06.12.2023.
+//
+
+import Foundation
