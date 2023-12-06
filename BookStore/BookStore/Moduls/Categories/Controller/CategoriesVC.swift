8eaa935397da0cfabfa7d5e56378937349d643f4
Maksimova Kristina <kris.maks.t.7@gmail.ru>
2023-12-06 21:42:26 +0300

    create CategoriesVC

--- BookStore/BookStore/Moduls/Categories/Controller/CategoriesVC.swift
+++ BookStore/BookStore/Moduls/Categories/Controller/CategoriesVC.swift
@@ -1,10 +1,59 @@
 import UIKit
 
 class CategoriesVC: UIViewController {
-
+    
+    //MARK: - CreateUIElements, private constans
+    private let categoriesView = CategoriesView()
+    private let widthItem = 150
+    private let heightItem = 100
+    private let spacingForSection = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-
+        view.backgroundColor = .systemGray
+        categoriesView.collectionView.delegate = self
+        categoriesView.collectionView.dataSource = self
+        view = categoriesView
     }
 }
+//MARK: - extensions CategoriesVC
+extension CategoriesVC: UICollectionViewDelegate,
+                        UICollectionViewDataSource,
+                        UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoriesCell.self.description(),
+                                                      for: indexPath) as! CategoriesCell
+        cell.clipsToBounds = true
+        cell.layer.cornerRadius = 5
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: CategoriesHeader.self.description(), for: indexPath) as! CategoriesHeader
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: widthItem, height: heightItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return spacingForSection
+    }
+}
+
+
+
---
+++ BookStore/BookStore/Moduls/Categories/View/CategoriesHeader.swift
@@ -0,0 +1,40 @@
+//
+//  CategoriesHeader.swift
+//  BookStore
+//
+//  Created by Кристина Максимова on 06.12.2023.
+//
+
+<<<<<<< HEAD
+import UIKit
+import SnapKit
+
+final class CategoriesHeader: UICollectionReusableView {
+    
+    //MARK: - UI Elements
+    lazy var headerLabel = UILabel.makeLabel(text: "Categories", font: UIFont.openSansRegular(ofSize: 20), textColor: .black)
+    
+    //MARK: - Inits
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Propetries
+    private func setupViews() {
+        addSubview(headerLabel)
+        headerLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+=======
+import Foundation
+>>>>>>> cf5fc0e60d5ebc34ab0693f5f1289ebf4f399058
