c1cef9f2052489890f102bd8348221d5ca7a4a93
michaelbolgar <mr.mbolgar@inbox.ru>
2023-12-09 21:51:07 +0100

    done Book Details Screen

---
+++ BookStore/BookStore/Moduls/BookDetails/Model/BookDetailsModel.swift
@@ -0,0 +1,18 @@
+//
+//  BookModel.swift
+//  BookStore
+//
+//  Created by Alexander Altman on 07.12.2023.
+//
+
+import UIKit
+
+// Mock Data
+struct BookDetailsModel {
+    static var authorName: String = "Oscar Wilde"
+    static var category: String = "Classics"
+    static var raiting: String = "4.11/5"
+    static var descriptionText: String = """
+Oscar Wilde’s only novel is the dreamlike story of a young man who sells his soul for eternal youth and beauty. In this celebrated work Wilde forged a devastating portrait of the effects of evil and debauchery on a young aesthete in late-19th-century England. Combining elements of the Gothic horror novel and decadent French fiction, the book centers on a striking premise: As Dorian Gray sinks into a life of crime and gross sensuality, his body retains perfect youth and vigor while his recently painted portrait grows day by day into a hideous record of evil, which he must keep hidden from the world. For over a century, this mesmerizing tale of horror and suspense has enjoyed wide popularity. It ranks as one of Wilde's most important creations and among the classic achievements of its kind.
+"""
+}
