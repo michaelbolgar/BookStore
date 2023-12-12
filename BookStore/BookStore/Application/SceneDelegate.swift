bed9c36ec14ada46285777360ba6024e71f5e090
Alexander Altman <qewhouse@gmail.com>
2023-12-12 18:01:45 +0300

    launch screen

--- BookStore/BookStore/Application/SceneDelegate.swift
+++ BookStore/BookStore/Application/SceneDelegate.swift
@@ -8,22 +8,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        //                let appLaunchedBefore = UserDefaults.standard.bool(forKey: "appLaunchedBefore")
-        //                if appLaunchedBefore {
-        //                    let mainVC = MainTabBarController()
-        //                    let navigationController = UINavigationController(rootViewController: mainVC)
-        //                    window?.rootViewController = navigationController
-        //                } else {
-        //                    let onboardingVC = OnboardingViewController()
-        //                    window?.rootViewController = onboardingVC
-        //                }
-        //                UserDefaults.standard.set(true, forKey: "appLaunchedBefore")
-        
         let savedDarkMode = UserDefaults.standard.bool(forKey: "AppDarkMode")
         let selectedTheme: UIUserInterfaceStyle = savedDarkMode ? .dark : .light
         window?.overrideUserInterfaceStyle = selectedTheme
+        window?.rootViewController = LaunchViewController()
         
-        window?.rootViewController = MainTabBarController()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+                self.window?.rootViewController = MainTabBarController()
+            //!!!: Delete line above anu uncomment lines 19-28 before release
+            //                let appLaunchedBefore = UserDefaults.standard.bool(forKey: "appLaunchedBefore")
+            //                if appLaunchedBefore {
+            //                    let mainVC = MainTabBarController()
+            //                    let navigationController = UINavigationController(rootViewController: mainVC)
+            //                    window?.rootViewController = navigationController
+            //                } else {
+            //                    let onboardingVC = OnboardingViewController()
+            //                    window?.rootViewController = onboardingVC
+            //                }
+            //                UserDefaults.standard.set(true, forKey: "appLaunchedBefore")
+            
+            }
         window?.makeKeyAndVisible()
     }
 }
